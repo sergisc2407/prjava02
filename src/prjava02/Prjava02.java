4ff471f1bbefe8210281cea016280bf2f579c0e6
sergisc2407 <sersac78@hotmail.com>
2019-05-02 10:56:40 +0200

    Treballant amb ordres Git i branques: Versió 1.2 del projecte prjava02

--- src/prjava02/Prjava02.java
+++ src/prjava02/Prjava02.java
@@ -22,7 +22,7 @@ public class Prjava02 {
             System.out.println("Carpeta Personal: " + System.getProperty("user.home"));
             System.out.println("Sistema operatiu: " + System.getProperty("os.name"));
             System.out.println("Versió OS: " + System.getProperty("os.version"));
-            System.out.println("Creació d'una branca del projecte prjava02");
+            System.out.println("Afegint més codi a la branca00 del projecte prjava02");
         } catch (IOException e) {
             System.out.println("Exception occurred");
         }
